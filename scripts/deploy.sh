568cdc6c8e3ff19a2fff50201c0a94796161e311
rajnishkumar4545 <rajnishku493@gmail.com>
2023-08-08 23:25:33 +0530

    Added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+if [[ $BUILD=1 ]]; then
+    echo Build started on $(date)
+    # install lambda dependecy, may be do it using venv
+    pip install -r src/lambda/requirements.txt -t src/lambda/
+    zip -r src/lambda/demo_lambda.zip src/lambda/
+    npm install -g aws-cdk
+    cdk bootstrap
+    cdk synth
+    zip -r artifacts.zip cdk.out
+    echo Build completed on `date`
+else
+    echo Deploy started in $(date)
+    ls -laRt
+    echo Deploy completed on $(date)
+fi
