0cff20968ae0768cc434975109a51c8dd739e6b9
rajnishkumar4545 <rajnishku493@gmail.com>
2023-08-09 00:14:57 +0530

    Fixing the deployment script logic

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-if [[ $BUILD=1 ]]; then
+if [[ $BUILD == 1 ]]; then
     echo Build started on $(date)
     # install lambda dependecy, may be do it using venv
     # pip install -r src/lambda/requirements.txt -t src/lambda/
@@ -8,7 +8,7 @@ if [[ $BUILD=1 ]]; then
     cdk synth
     zip -r artifacts.zip cdk.out
     echo Build completed on `date`
-elif [[ $BUILD=0 ]] then
+elif [[ $BUILD == 0 ]]; then
     echo Deploy started in $(date)
     ls -laRt
     echo Deploy completed on $(date)
